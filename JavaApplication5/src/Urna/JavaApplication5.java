bf48e788d85a127b733d24df98977653bccef40f
Carloseducustodio <33076872+Carloseducustodio@users.noreply.github.com>
2017-12-01 20:26:22 -0200

    Add files via upload

---
+++ JavaApplication5/src/Urna/JavaApplication5.java
@@ -0,0 +1,15 @@
+
+package Urna;
+
+
+public class JavaApplication5 {
+
+  
+    public static void main(String[] args) {
+      
+        ClasseUrna menu = new ClasseUrna();
+        menu.MenuOpcoes();
+           
+    }
+    
+}
